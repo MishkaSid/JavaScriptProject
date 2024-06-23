db1c4b6c12407e52cc6db5cfae37adecc60e1870
MishkaSid <sidorukmichael@gmail.com>
2024-06-23 17:01:54 +0300

    JS.js

---
+++ JS.js
@@ -0,0 +1,17 @@
+`use strict`
+
+let ButtonInfo = document.getElementsByClassName("ButtonInfo");
+let ButtonEdit = document.getElementById("ButtonEdit");
+let ButtonTrash = document.getElementById("ButtonTrash");
+
+
+function OnClickInfo() {
+   alert("no info yet");
+}
+
+function OnClickEdit() {
+    alert("no edit yet");
+}
+function OnClickTrash() {
+   alert("no delete yet");
+}
